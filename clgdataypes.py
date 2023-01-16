df604bc1ba06b5e4e3db3f67d3a7a3b8336d0a5f
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-01-16 15:37:04 +0530

    Create clgdataypes.py

---
+++ clgdataypes.py
@@ -0,0 +1,17 @@
+# Declare variables of different data types
+integer_var = 5
+float_var = 3.14
+string_var = "Hello World"
+boolean_var = True
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+
+# Print the variables and their data types
+print("Integer variable: ", integer_var, " Data type: ", type(integer_var))
+print("Float variable: ", float_var, " Data type: ", type(float_var))
+print("String variable: ", string_var, " Data type: ", type(string_var))
+print("Boolean variable: ", boolean_var, " Data type: ", type(boolean_var))
+print("List variable: ", list_var, " Data type: ", type(list_var))
+print("Tuple variable: ", tuple_var, " Data type: ", type(tuple_var))
+print("Dictionary variable: ", dict_var, " Data type: ", type(dict_var))
